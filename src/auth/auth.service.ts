d90ee0e0532412f089d4b2d8452c643bc3919490
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-12-11 13:45:07 +0200

    auth dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-export class AuthService{
-  constructor(private prisma:PrismaService){}
+export class AuthService {
+  constructor(private prisma: PrismaService) { }
   signin() {
     return 'i am signed in';
   }
-  signup() { 
+  signup() {
     return 'i am signed up';
   }
 }
