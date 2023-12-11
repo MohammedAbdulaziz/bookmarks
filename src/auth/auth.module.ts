cf3e48854b6d7d8353b943fd7dc9611e973ccd62
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-12-11 12:27:56 +0200

    prisma inits

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
-@Module({})
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+@Module({
+  imports: [PrismaModule],
+  controllers: [
+    AuthController
+  ],
+  providers: [
+    AuthService
+  ],
+  exports: []
+})
 export class AuthModule {
 }
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AuthService{
+  constructor(private prisma:PrismaService){}
+  signin() {
+    return 'i am signed in';
+  }
+  signup() { 
+    return 'i am signed up';
+  }
+}
